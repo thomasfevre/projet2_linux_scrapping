3479cc53f92ead3235e910153b4abd2faae7da64
thomasfevre <73887758+thomasfevre@users.noreply.github.com>
2022-12-17 13:55:46 +0100

    adding new working files

---
+++ telegram_bot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+GROUP_ID="-1001768659970"
+BOT_TOKEN=5849916614:AAFQcEmjtVBKlm4dJh3Xq0FND3RuRML_cOA
+
+curl -S --data "text=$1" "https://api.telegram.org/bot5849916614:AAFQcEmjtVBKlm4dJh3Xq0FND3RuRML_cOA/sendMessage?chat_id=-1001768659970"
