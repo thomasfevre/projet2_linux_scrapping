756a6e92cedc7f050fbda65e429c2a98f2b6331b
thomasfevre <73887758+thomasfevre@users.noreply.github.com>
2022-12-17 14:04:21 +0100

    sec

--- telegram_bot.sh
+++ telegram_bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GROUP_ID="-1001768659970"
-BOT_TOKEN=5849916614:AAFQcEmjtVBKlm4dJh3Xq0FND3RuRML_cOA
+GROUP_ID=
+BOT_TOKEN=
 
 curl -S --data "text=$1" "https://api.telegram.org/bot5849916614:AAFQcEmjtVBKlm4dJh3Xq0FND3RuRML_cOA/sendMessage?chat_id=-1001768659970"
